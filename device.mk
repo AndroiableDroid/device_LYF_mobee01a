af5acc1c60e3aeee38f91bc2f2b7f502139db181
Mohd Faraz <androiabledroid@gmail.com>
2018-08-16 16:23:28 +0000

    mobee01a: Convert to own module As we have modified the original aw2013 hal for my device.
    Signed-off-by: Mohd Faraz <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -76,7 +76,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.aw2013
+    android.hardware.light@2.0-service.mobee01a
 
 # Media
 PRODUCT_COPY_FILES += \
