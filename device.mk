9ba9aeb38e92fa1f7be4187dfa0d4947ceff036f
AndroiableDroid <androiabledroid@gmail.com>
2019-02-14 21:07:35 +0530

    mobee01a: Added missed hals.conf
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -106,7 +106,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service.mobee01a \
-    sensors.msm8916
+    sensors.msm8916 \
+    hals.conf
 
 # Thermal
 PRODUCT_COPY_FILES += \
