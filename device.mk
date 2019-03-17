b8d36379c8b242b8059867999bb2903aa3331ef4
AndroiableDroid <androiabledroid@gmail.com>
2019-03-17 19:40:55 +0530

    mobee01a: mv gps to common
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -53,16 +53,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@1.0-impl
 
-# GPS
-PRODUCT_PACKAGES += \
-    gps.msm8916
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
-    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
-    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
-
 # Init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
