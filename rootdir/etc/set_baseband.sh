ed8ad9993bb34adb8411f78cb46185fbd54ec25b
AndroiableDroid <androiabledroid@gmail.com>
2019-03-20 00:01:32 +0530

    mobee01a: Update baseband path
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- rootdir/etc/set_baseband.sh
+++ rootdir/etc/set_baseband.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
 # grep the modem partition for baseband version and set it
-setprop gsm.version.baseband `strings /firmware/image/modem.b18 | grep "^MPSS.DPM" | head -1`
+setprop gsm.version.baseband `strings /dev/block/bootdevice/by-name/modem | grep "^MPSS.DPM" | head -1`
