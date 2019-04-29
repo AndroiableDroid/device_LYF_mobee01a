0d1bd94e25a2e790db7df898c69891e85e95adf1
AndroiableDroid <androiabledroid@gmail.com>
2019-04-29 19:33:42 +0530

    mobee01a: Build sensors.native
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -84,6 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service.mobee01a \
     sensors.msm8916 \
+    sensors.native \
     hals.conf
 
 # Thermal
