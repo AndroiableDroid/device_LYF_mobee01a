963fffc12481dcec7158d241a73cdf4b65905f6e
AndroiableDroid <androiabledroid@gmail.com>
2019-04-30 19:50:08 +0530

    LS-5015: consider as AOSP
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- lineage_mobee01a.mk
+++ lineage_mobee01a.mk
@@ -16,7 +16,7 @@
 $(call inherit-product, device/LYF/mobee01a/full_mobee01a.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
