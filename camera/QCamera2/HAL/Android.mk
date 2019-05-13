612ec25a1b6c7343f9ee8b5f5ffe4794a5bf3d3e
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-05-13 11:09:53 +0000

    mobee01a: camera: Clean
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -71,16 +71,13 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/tsMakeuplib/include
 endif
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
-LOCAL_SHARED_LIBRARIES := libEGL liblog libhardware libutils libcutils libdl libsync
+LOCAL_SHARED_LIBRARIES := libEGL liblog libhardware libutils libcutils libdl
 LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libqdMetaData
-LOCAL_SHARED_LIBRARIES += libhidltransport android.hidl.token@1.0-utils android.hardware.graphics.bufferqueue@1.0
 ifeq ($(TARGET_TS_MAKEUP),true)
 LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 LOCAL_HEADER_LIBRARIES := media_plugin_headers
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
-LOCAL_STATIC_LIBRARIES := libarect libbase
-
 LOCAL_HEADER_LIBRARIES += media_plugin_headers
 
 LOCAL_HEADER_LIBRARIES += libnativebase_headers libhardware_headers
