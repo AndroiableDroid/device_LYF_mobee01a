0cf1471f28681f730ffb5f4ef9e8cd61a4c5de01
Mohd Faraz <mohd.faraz.abc@gmail.com>
2019-07-26 21:21:31 +0530

    mobee01a: Generic config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 LZMA_RAMDISK_TARGETS := recovery
 TARGET_KERNEL_SOURCE := kernel/LYF/mobee01a
-TARGET_KERNEL_CONFIG := destructive_defconfig
+TARGET_KERNEL_CONFIG := lineage_mobee01a_defconfig
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
